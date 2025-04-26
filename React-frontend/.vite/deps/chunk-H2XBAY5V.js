70d2d81fc37106025a7a74c98acfaa6594c389ce
dilaksha Lakshan <dilakshalakshan2001@gmail.com>
2025-04-26 13:08:18 +0530

    13

---
+++ React-frontend/.vite/deps/chunk-H2XBAY5V.js
@@ -0,0 +1,23 @@
+import {
+  _setPrototypeOf,
+  init_setPrototypeOf
+} from "./chunk-RA4UCUOC.js";
+import {
+  __esm
+} from "./chunk-V4OQ3NZ2.js";
+
+// node_modules/@babel/runtime/helpers/esm/inheritsLoose.js
+function _inheritsLoose(t, o) {
+  t.prototype = Object.create(o.prototype), t.prototype.constructor = t, _setPrototypeOf(t, o);
+}
+var init_inheritsLoose = __esm({
+  "node_modules/@babel/runtime/helpers/esm/inheritsLoose.js"() {
+    init_setPrototypeOf();
+  }
+});
+
+export {
+  _inheritsLoose,
+  init_inheritsLoose
+};
+//# sourceMappingURL=chunk-H2XBAY5V.js.map
